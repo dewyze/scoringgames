06c58b65e238c357708061fde55b4f13dd49d979
John DeWyze <1312168+dewyze@users.noreply.github.com>
2019-10-11 11:46:10 -0500

    Add surrounding container

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import './main.css';
-import { Elm } from './Main.elm';
-import * as serviceWorker from './serviceWorker';
+import "./main.css";
+import { Elm } from "./Main.elm";
+import * as serviceWorker from "./serviceWorker";
 
 Elm.Main.init({
-  node: document.getElementById('root')
+  node: document.getElementById("root")
 });
+document.getElementById("main").style.height = window.innerHeight + "px";
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
