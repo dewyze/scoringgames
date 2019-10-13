02f1a410e73eedd7d6efddc0e85a93fabe26536f
John DeWyze <1312168+dewyze@users.noreply.github.com>
2019-10-12 19:33:10 -0500

    Remove ID from session

--- src/Session.elm
+++ src/Session.elm
@@ -5,12 +5,10 @@ import Browser.Navigation as Nav
 
 type alias Session =
     { navKey : Nav.Key
-    , id : String
     }
 
 
 init : Nav.Key -> Session
 init key =
     { navKey = key
-    , id = "123"
     }
